ed673e89bfc2392a8fb534c00d3aea3f427dccee
Mattk205 <mattk205@gmail.com>
2017-08-30 17:36:45 -0400

    assignment 5 complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ require_relative '../models/address_book'
    def main_menu
      puts "Main Menu - #{address_book.entries.count} entries"
      puts "1 - View all entries"
-     puts "2 - Create an entry"
-     puts "3 - Search for an entry"
-     puts "4 - Import entries from a CSV"
-     puts "5 - Exit"
+     puts "2 - View entry number n"
+     puts "3 - Create an entry"
+     puts "4 - Search for an entry"
+     puts "5 - Import entries from a CSV"
+     puts "6 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -25,17 +26,21 @@ require_relative '../models/address_book'
          main_menu
        when 2
          system "clear"
-         create_entry
+         view_entry_n
          main_menu
        when 3
          system "clear"
-         search_entries
+         create_entry
          main_menu
        when 4
          system "clear"
-         read_csv
+         search_entries
          main_menu
        when 5
+         system "clear"
+         read_csv
+         main_menu
+       when 6
          puts "Good-bye!"
          # #8
          exit(0)
@@ -57,6 +62,21 @@ require_relative '../models/address_book'
 
      system "clear"
      puts "End of entries"
+   end
+
+   def view_entry_n
+     print "Entry number to view: "
+     selection = gets.chomp.to_i
+
+     if selection <= address_book.entries.count
+       puts address_book.entries[selection]
+       puts "press enter to return to main menu"
+       gets.chomp
+       system "clear"
+     else
+       puts "#{selection} is not a valid input"
+       view_entry_n
+     end
    end
 
    def create_entry
